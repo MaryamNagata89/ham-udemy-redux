04e86fa6f0451ff8964d0b90668d526232636c9b
永田マリ <nagamari80@gmail.com>
2020-06-07 20:43:27 +0900

    conponent props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React,{ Component } from 'react';
 
+import React, { Component } from 'react';
 
 class App extends Component {
-  render(){
-	return (
-<> 
-<label htmlFor="bar">bar</label>
-<input type ="text"onClick={()=>{console.log('im clicked')}}/>
-      
-</>
-);
+  render() {
+    return (
+      <>
+        <label htmlFor="bar">bar</label>
+        <input
+          type="text"
+          onClick={() => {
+            console.log('im clicked');
+          }}
+        />
+        <User name="Maryam" age={36} />
+      </>
+    );
+  }
+}
+class User extends Component {
+  constructor(props) {
+    super(props);
+    this.name = props.name;
+    this.age = props.age;
+  }
+  render() {
+    return (
+      <div>
+        <p>
+          Im {this.name} and my age is {this.age}
+        </p>
+      </div>
+    );
+  }
 }
 
-}
 export default App;
+
